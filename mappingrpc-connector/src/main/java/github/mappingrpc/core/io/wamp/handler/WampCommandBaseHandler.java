c5f3843d34f60c6014dfced9e425ddd7fccd493f
zhoufeng <zhoufenglokki@gmail.com>
2014-05-03 16:23:44 +0800

    log exception

--- mappingrpc-connector/src/main/java/github/mappingrpc/core/io/wamp/handler/WampCommandBaseHandler.java
+++ mappingrpc-connector/src/main/java/github/mappingrpc/core/io/wamp/handler/WampCommandBaseHandler.java
@@ -4,13 +4,19 @@ import github.mappingrpc.core.io.wamp.constant.MsgTypeConstant;
 import github.mappingrpc.core.metadata.MetaHolder;
 import io.netty.channel.ChannelHandlerContext;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import com.alibaba.fastjson.JSONArray;
 
 public class WampCommandBaseHandler implements Runnable {
+
+	static Logger log = LoggerFactory.getLogger(WampCommandBaseHandler.class);
+
 	private MetaHolder metaHolder;
 	private JSONArray jsonArray;
 	private ChannelHandlerContext channelCtx;
-	
+
 	public WampCommandBaseHandler(MetaHolder metaHolder, ChannelHandlerContext channelCtx, JSONArray jsonArray) {
 		this.metaHolder = metaHolder;
 		this.channelCtx = channelCtx;
@@ -20,6 +26,14 @@ public class WampCommandBaseHandler implements Runnable {
 	@Override
 	public void run() {
 		int commandType = jsonArray.getIntValue(0);
+		try {
+			runInCatch(commandType);
+		} catch (Throwable ex) {
+			log.error("{commandType:" + commandType + "}", ex);
+		}
+	}
+
+	private void runInCatch(int commandType) {
 		switch (commandType) {
 		case MsgTypeConstant.call:
 			CallCommandHandler.processCommand(metaHolder, channelCtx, jsonArray);
